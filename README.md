8b7620b5a688a12b1e42db76ce80778a53eea22b
Daniel Galvez <dgalvez@nvidia.com>
2023-12-11 11:21:15 -0800

    Reproduction instructions

---
+++ README.md
@@ -0,0 +1,11 @@
+# Instructions
+
+All experiments were run on A100-80GB.
+
+
+To reproduce the RTFx numbers and word error rates in the report, do:
+
+```
+pip install -r requirements.txt
+bash whisper_experiments/run_all.sh
+```
--- whisper_experiments/run_none_prompt.sh
+++ whisper_experiments/run_none_prompt.sh
@@ -1,7 +1,7 @@
 echo "None prompt"
 
-# python create_prompts.py --prompt_type=none --output_file_json=output_prompts/none_prompt.json
+python create_prompts.py --prompt_type=none --output_file_json=output_prompts/none_prompt.json
 
-# python whisper_experiments/whisper_example3.py --file_to_prompt_path=output_prompts/none_prompt.json --hypotheses_file=output_prompts/none_hyps.txt --references_file=output_prompts/none_refs.txt
+python whisper_experiments/whisper_example3.py --file_to_prompt_path=output_prompts/none_prompt.json --hypotheses_file=output_prompts/none_hyps.txt --references_file=output_prompts/none_refs.txt
 
 python whisper_experiments/whisper_example3.py --file_to_prompt_path=output_prompts/none_prompt.json --hypotheses_file=output_prompts/none_append_hyps.txt --references_file=output_prompts/none_append_refs.txt --append_previous_transcripts_to_prompt=True
